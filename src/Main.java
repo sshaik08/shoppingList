8cbc015490743983350992eb5ff23ab5dc257450
shqzia <117523930+sshaik08@users.noreply.github.com>
2025-09-22 12:17:15 -0400

    added documentation

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,10 @@ public class Main {
         Scanner input = new Scanner(System.in);
         LinkedList<String> shoppingList = new LinkedList<>();
 
+        // creating the text file for the shopping list
         File file = new File("shoppingList.txt");
 
+        // asks the user if they are opening an existing shopping list or beginning a new one
         System.out.print("Enter 'o' to open your previous shopping list or enter 'n' to write a new one: ");
         String choice1 = input.nextLine();
         if (choice1.equals("n")) {
@@ -20,6 +22,7 @@ public class Main {
             System.out.println("invalid response");
         }
 
+        // continuously asks the user if they want to add or remove an item to the list until they quit
         String choice2;
         while(true) {
             System.out.print("Enter 'r' to remove an item, 'a' to add an item, or 'q' to quit: ");
@@ -41,6 +44,10 @@ public class Main {
         }
     }
 
+    /**
+     * Saves the shopping list in the .txt file line by line
+     * @param shoppingList the linkedList in which the items of the shopping list are saved
+     */
     public static void saveList(LinkedList<String> shoppingList) {
         try {
             FileWriter fw = new FileWriter("shoppingList.txt");
@@ -53,12 +60,21 @@ public class Main {
         }
     }
 
+    /**
+     * Loads the shopping list as a linked list object by reading each separate line in the txt file and adding
+     * the content of the line to the shopping list object
+     * @return the shopping list in a linked list object that can be read by the java program
+     * @throws IOException This exception is thrown if the shoppingList.txt file is not found
+     */
     public static LinkedList<String> loadList() throws IOException {
         LinkedList<String> shoppingList = new LinkedList<>();
-
-        FileReader fr = new FileReader("shoppingList.txt");
-        BufferedReader br = new BufferedReader(fr);
-
+        BufferedReader br = null;
+        try {
+            FileReader fr = new FileReader("shoppingList.txt");
+            br = new BufferedReader(fr);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
         String line;
         while((line = br.readLine()) != null) {
             shoppingList.add(line);
@@ -66,6 +82,12 @@ public class Main {
         return shoppingList;
     }
 
+    /**
+     * Writes a new shopping list, taking in input from the user to make the list a certain size and adding that number
+     * of items from the user
+     * @param input the int value of the size of the linked list and the String values of each item added to the linked list
+     * @param shoppingList the shopping list in a linked list object that can be read by the java program
+     */
     public static void newList(Scanner input, LinkedList<String> shoppingList) {
         System.out.print("Enter the number of items you wish to add: ");
         int numItems = input.nextInt();
@@ -80,6 +102,11 @@ public class Main {
 
     }
 
+    /**
+     * Removes an item from the shopping list object given the index of the item
+     * @param input the index of the item to be removed from the linked list
+     * @param shoppingList the shopping list in a linked list object that can be read by the java program
+     */
     public static void removeItem(Scanner input, LinkedList<String> shoppingList) {
         System.out.println("Current list: " + "\n" + shoppingList);
         System.out.print("Enter the index (begins at 1) of the item to be removed from the list: ");
@@ -92,6 +119,11 @@ public class Main {
         }
     }
 
+    /**
+     * Adds an item to end of the shopping list object given the String value of the item
+     * @param input the String value of the item to be added to the linked list
+     * @param shoppingList the shopping list in a linked list object that can be read by the java program
+     */
     public static void addItem(Scanner input, LinkedList<String> shoppingList) {
         System.out.println("Current list: " + "\n" + shoppingList);
         System.out.print("Enter the name of the item to be added to the list: ");
